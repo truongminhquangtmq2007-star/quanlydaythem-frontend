c925d6d22a7b89ccacd3be0f70d0b509a0bf88aa
truongminhquangtmq2007-star <truongminhquangtmq2007@gmail.com>
2026-08-24 16:46:44 +0700

    chore: remove patch scripts

--- fix_student_type.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/types/core.ts', 'utf8');
-if (!code.includes('ai_evaluation')) {
-  code = code.replace(/learning_goals\?: string;/g, "learning_goals?: string;\n  ai_evaluation?: any;");
-  fs.writeFileSync('src/types/core.ts', code);
-}
-
